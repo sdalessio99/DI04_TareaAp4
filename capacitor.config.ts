23083cdc537ada3d51c47ff2c11394b28cbc4bc2
Sara D'Alessio <sdalessio@birt.eus>
2024-03-01 19:07:47 +0100

    Commit clonacion y app funciona

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'DI04_TA2',
+  appName: 'DI04_TA3',
   webDir: 'www',
   server: {
     androidScheme: 'https'
